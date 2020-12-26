cfb8f3fe411693ea694c315442862eb2fb11437b
cloudwebrtc <duanweiwei1982@gmail.com>
2020-12-26 23:22:27 +0800

    Add push payload format to README.md.

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # callkeep
+
 * iOS CallKit and Android ConnectionService for Flutter
 * Support FCM and PushKit
+
+## push payload
+
+```json
+{
+    "callkeep": {
+        "uuid": "xxxxx-xxxxx-xxxxx-xxxxx",
+        "caller_id": "+8618612345678",
+        "caller_name": "hello",
+        "caller_id_type": "number", 
+        "has_video": false,
+    },
+    "extra": {
+        "foo": "bar",
+        "key": "value",
+    }
+}
+```
--- ios/Classes/CallKeep.m
+++ ios/Classes/CallKeep.m
@@ -224,32 +224,38 @@ static CXProvider* sharedProvider;
     // Process the received push
     NSLog(@"didReceiveIncomingPushWithPayload payload = %@", payload.type);
     /* payload example.
-     {
-         "callkeep": {
-             "caller_id": "+8618612345678",
-             "caller_name": "hello",
-             "caller_id_type": "number",
-             "has_video": false,
-         },
-         "extra": {
-             "foo": "bar",
-             "key": "value",
-         }
-     }
+    {
+        "callkeep": {
+            "uuid": "xxxxx-xxxxx-xxxxx-xxxxx",
+            "caller_id": "+8618612345678",
+            "caller_name": "hello",
+            "caller_id_type": "number", 
+            "has_video": false,
+        },
+        "extra": {
+            "foo": "bar",
+            "key": "value",
+        }
+    }
     */
     NSDictionary *dic = payload.dictionaryPayload[@"callkeep"];
-    NSString *number = dic[@"caller_id"];
-    NSString *localizedCallerName = dic[@"caller_name"];
+    NSString *callerId = dic[@"caller_id"];
+    NSString *callerName = dic[@"caller_name"];
     BOOL hasVideo = [dic[@"has_video"] boolValue];
-    NSString *handleType = dic[@"caller_id_type"];
-    
+    NSString *callerIdType = dic[@"caller_id_type"];
+    NSString *uuid = dic[@"uuid"];
+
+    if( uuid == nil) {
+        uuid = [self createUUID];
+    }
+
     //NSDictionary *extra = payload.dictionaryPayload[@"extra"];
 
-    [CallKeep reportNewIncomingCall:[self createUUID]
-                             handle:number
-                         handleType:handleType
+    [CallKeep reportNewIncomingCall:uuid
+                             handle:callerId
+                         handleType:callerIdType
                            hasVideo:hasVideo
-                localizedCallerName:localizedCallerName
+                localizedCallerName:callerName
                         fromPushKit:YES
                             payload:payload.dictionaryPayload
               withCompletionHandler:^(){}];
