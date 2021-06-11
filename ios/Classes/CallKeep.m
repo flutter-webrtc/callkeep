50652c4c66913877bec2cb5fd69b5ecc99a5b5be
cloudwebrtc <duanweiwei1982@gmail.com>
2021-06-12 01:26:42 +0800

    When using the alert format, print the log and terminate the calkeep process.

--- ios/Classes/CallKeep.m
+++ ios/Classes/CallKeep.m
@@ -232,7 +232,13 @@ static CXProvider* sharedProvider;
         "has_video": false,
     }
     */
+
     NSDictionary *dic = payload.dictionaryPayload;
+
+    if (dic[@"aps"] != nil) {
+        NSLog(@"Do not use the 'alert' format for push type %@.", payload.type);
+        return;
+    }
 
     NSString *uuid = dic[@"uuid"];
     NSString *callerId = dic[@"caller_id"];
