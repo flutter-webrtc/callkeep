2304f32e4dde9792f544e5413c623d1b9b0f96cc
sfolador <folador@medacta.ch>
2022-02-14 10:39:25 +0100

    Added payload management for CallKeepDidDisplayIncomingCall

--- lib/src/actions.dart
+++ lib/src/actions.dart
@@ -40,12 +40,14 @@ class CallKeepDidDisplayIncomingCall extends EventType {
         handle = arguments['handle'] as String,
         localizedCallerName = arguments['localizedCallerName'] as String,
         hasVideo = arguments['hasVideo'] as bool,
-        fromPushKit = arguments['fromPushKit'] as bool;
+        fromPushKit = arguments['fromPushKit'] as bool,
+        payload = arguments['payload'] as Map<dynamic,dynamic>;
   String? callUUID;
   String? handle;
   String? localizedCallerName;
   bool? hasVideo;
   bool? fromPushKit;
+  Map<dynamic,dynamic>? payload;
 }
 
 class CallKeepDidPerformSetMutedCallAction extends EventType {
