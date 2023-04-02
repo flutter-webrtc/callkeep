b500ab96889e7d92ecb80dd9ebfe694a91347195
ryojiro <ryojirok@gmail.com>
2023-04-02 10:09:54 +0900

    example: Remove space.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,7 +15,7 @@ bool _callKeepInited = false;
     "uuid": "xxxxx-xxxxx-xxxxx-xxxxx",
     "caller_id": "+8618612345678",
     "caller_name": "hello",
-    "caller_id_type": "number", 
+    "caller_id_type": "number",
     "has_video": false,
 
     "extra": {
