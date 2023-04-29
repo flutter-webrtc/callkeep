ad91957ea2d98dd69a73588904ee631760c5160a
senkia24 <nyamdavaa@callpro.mn>
2023-04-29 17:52:37 +0800

    test

--- android/src/main/java/io/wazo/callkeep/MyInCallService.java
+++ android/src/main/java/io/wazo/callkeep/MyInCallService.java
@@ -3,8 +3,11 @@ package io.wazo.callkeep;
 import android.telecom.InCallService;
 
 public class MyInCallService extends InCallService {
-    @Override
-    public void setAudioRoute(int route){
-        super.setAudioRoute(route);
+    public void setRoute(int route) {
+        CallAudioState currentAudioState = getCallAudioState();
+        CallAudioState newAudioState = new CallAudioState(currentAudioState.isMuted(), route,
+                    conn.getCallAudioState().getSupportedRouteMask());
+
+        onCallAudioStateChanged(newAudioState);
     }
 }
