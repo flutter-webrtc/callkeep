b3b60dbbe4e8479d17c74bb50565c5ac1846d895
senkia24 <nyamdavaa@callpro.mn>
2023-04-29 17:05:17 +0800

    test

--- android/src/main/java/io/wazo/callkeep/MyInCallService.java
+++ android/src/main/java/io/wazo/callkeep/MyInCallService.java
@@ -1,9 +1,13 @@
 package io.wazo.callkeep;
-
 import android.telecom.InCallService;
 
+// @see https://github.com/kbagchiGWC/voice-quickstart-android/blob/9a2aff7fbe0d0a5ae9457b48e9ad408740dfb968/exampleConnectionService/src/main/java/com/twilio/voice/examples/connectionservice/VoiceConnectionService.java
+@TargetApi(Build.VERSION_CODES.M)
 public class MyInCallService extends InCallService {
-    private void setAudioRoute(int route) {
+    public MyInCallService() {
+    }
+
+    public static void setAudioRoute(int route) {
         if (getCallAudioState() != null) {
             getCallAudioState().setRoute(route);
         }
