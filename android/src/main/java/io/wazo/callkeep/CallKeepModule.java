2f2a22a3fc67094165dae65b372be8287422f500
Nyamdavaa <nyamdavaa@callpro.mn>
2023-09-28 16:17:50 +0800

    space

--- android/src/main/java/io/wazo/callkeep/CallKeepModule.java
+++ android/src/main/java/io/wazo/callkeep/CallKeepModule.java
@@ -66,6 +66,7 @@ import io.wazo.callkeep.utils.PermissionUtils;
 
 import static io.wazo.callkeep.Constants.*;
 
+
 // @see https://github.com/kbagchiGWC/voice-quickstart-android/blob/9a2aff7fbe0d0a5ae9457b48e9ad408740dfb968/exampleConnectionService/src/main/java/com/twilio/voice/examples/connectionservice/VoiceConnectionServiceActivity.java
 public class CallKeepModule {
     public static final int REQUEST_READ_PHONE_STATE = 1337;
